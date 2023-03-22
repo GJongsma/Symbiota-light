095be5dd6c1c0a839eeaeeaf3bcb5b37c5b58435
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-03-22 08:57:44 -0700

    Add logos to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,9 @@
 <footer>
 	<p>© 2022 New Brunswick Museum / Musée du Nouveau-Brunswick</p>
+	<p style="margin:20px 250px 20px 250px">
+				<a href="https://www.nbm-mnb.ca/collections-and-research/" target="_blank" title="New Brunswick Museum / Musée du Nouveau-Brunswick"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NBM_White-325.png" style="height:50px" /></a>
+				<a href="https://www.nbwtf.ca/en/" target="_blank" title="New Brunswick Wildlife Trust Fund"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NB_WTF.png" style="height:70px" /></a>
+				<span style="margin-left: 600px">&nbsp;</span>
+			</p>
 	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
 </footer>
