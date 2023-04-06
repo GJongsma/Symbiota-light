f54bb13830c2fcc5d10083fcbc05aace5473ef7b
Edward Gilbert <egbot@asu.edu>
2023-04-06 09:03:44 -0700

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -11,6 +11,9 @@
 //$CUSTOM_CSS_PATH = '/css/symb/custom';
 ?>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
-<link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
+
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/symb/custom/main.css?ver=2" type="text/css" rel="stylesheet">
+
+<script src="<?php echo $CLIENT_ROOT; ?>/js/symb/lang.js" type="text/javascript"></script>
+
