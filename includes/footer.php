fb8e52f47034ad32c59bb7f06f0a590647404419
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-09-19 06:06:14 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,6 @@
 				<a href="https://www.nbwtf.ca/en/" target="_blank" title="New Brunswick Wildlife Trust Fund"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NB_WTF.png" style="margin-left:10px;height:70px" /></a>
 				<a href="http://www.accdc.com/" target="_blank" title="Atlantic Canada Conservation Data Centre"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/ACCDC_logo_nobkgr_2014.png" style="margin-left:10px;height:70px" /></a>
 				<a href="https://www.canada.ca/" target="_blank" title="Government of Canada"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CanadaGovLogo.svg" style="margin-left:10px;height:60px" /></a>
-				<span style="margin-left: 600px">&nbsp;</span>
 			</p>
 	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
 </footer>
