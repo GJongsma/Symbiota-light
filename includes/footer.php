b065681f534ffc082adf5a04a324a9bfedee7cdb
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-09-19 06:06:53 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<p>© 2022 New Brunswick Museum / Musée du Nouveau-Brunswick</p>
-	<p style="margin:20px 250px 20px 250px">
+	<p style="margin:20px 150px 20px 150px">
 				<a href="https://www.nbm-mnb.ca/collections-and-research/" target="_blank" title="New Brunswick Museum / Musée du Nouveau-Brunswick"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NBM_Black.png" style="height:50px" /></a>
 				<a href="https://www.nbwtf.ca/en/" target="_blank" title="New Brunswick Wildlife Trust Fund"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NB_WTF.png" style="margin-left:10px;height:70px" /></a>
 				<a href="http://www.accdc.com/" target="_blank" title="Atlantic Canada Conservation Data Centre"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/ACCDC_logo_nobkgr_2014.png" style="margin-left:10px;height:70px" /></a>
