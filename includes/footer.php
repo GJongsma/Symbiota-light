b1433eb34ea9773eb37d91ed40a98f86f53d9324
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-09-19 06:09:49 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 				<a href="https://www.nbm-mnb.ca/collections-and-research/" target="_blank" title="New Brunswick Museum / Musée du Nouveau-Brunswick"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NBM_Black.png" style="height:50px" /></a>
 				<a href="https://www.nbwtf.ca/en/" target="_blank" title="New Brunswick Wildlife Trust Fund"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NB_WTF.png" style="margin-left:10px;height:70px" /></a>
 				<a href="http://www.accdc.com/" target="_blank" title="Atlantic Canada Conservation Data Centre"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/ACCDC_logo_nobkgr_2014.png" style="margin-left:10px;height:70px" /></a>
-				<a href="https://www.canada.ca/" target="_blank" title="Government of Canada"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CanadaGovLogo.svg" style="margin-left:15px;height:50px" /></a>
+				<a href="https://www.canada.ca/en/canadian-heritage/services/funding/museums-assistance.html" target="_blank" title="Canadian Heritage Museum Assistance"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CanadaGovLogo.svg" style="margin-left:15px;height:50px" /></a>
 			</p>
 	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
 </footer>
