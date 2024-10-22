8626d55e7bf05599d3d4904bb2b2de02e09ad26a
Edward Gilbert <egbot@asu.edu>
2024-10-22 08:57:03 -0700

    add custom files

--- includes/footer.php
+++ includes/footer.php
@@ -6,5 +6,5 @@
 				<a href="http://www.accdc.com/" target="_blank" title="Atlantic Canada Conservation Data Centre"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/ACCDC_logo_nobkgr_2014.png" style="margin-left:10px;height:70px" /></a>
 				<a href="https://www.canada.ca/en/canadian-heritage/services/funding/museums-assistance.html" target="_blank" title="Canadian Heritage Museum Assistance"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CanadaGovLogo.svg" style="margin-left:15px;height:50px" /></a>
 			</p>
-	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
+	<p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a></p>
 </footer>
