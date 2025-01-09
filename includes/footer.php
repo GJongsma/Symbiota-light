9f3875a600715c4a4abcda92354c05b89ea90b7e
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-01-09 09:34:31 -0800

    Fix footer date and homepage translations

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<p>© 2022 New Brunswick Museum / Musée du Nouveau-Brunswick</p>
+	<p>© <?= date("Y"); ?> New Brunswick Museum / Musée du Nouveau-Brunswick</p>
 	<p style="margin:20px 150px 20px 150px">
 				<a href="https://www.nbm-mnb.ca/collections-and-research/" target="_blank" title="New Brunswick Museum / Musée du Nouveau-Brunswick"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NBM_Black.png" style="height:50px" /></a>
 				<a href="https://www.nbwtf.ca/en/" target="_blank" title="New Brunswick Wildlife Trust Fund"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/NB_WTF.png" style="margin-left:10px;height:70px" /></a>
--- index.php
+++ index.php
@@ -26,16 +26,37 @@ $countRecords = new CountOccurrenceRecords();
 	</div>
 	<div class="navpath"></div>
 	<div id="innertext">
+	<?php
+		if($LANG_TAG == 'fr'){
+			?>
+			<div>
+			<h1 class="headline">Histoire naturelle</h1>
+				<p>Les collections de recherche scientifiques du Musée du Nouveau-Brunswick sont le résultat d'un travail s'échelonnant 
+					sur plus d'un siècle et demi. Depuis ses débuts en 1842, comme Musée Gesner, les collections sont passées de 2 173 
+					objets énumérés dans le catalogue Gesner à plus de 300 000 spécimens aujourd'hui.</p>
+		</div>
+		<div style="width:400px;border: 15px solid #236192;padding:30px;margin:20px auto;">
+			Ce portail comprend actuellement:
+			<li>
+				<b><?= number_format($countRecords->countPlants()) ?></b> enregistrements de plantes
+			</li>
+			<li>
+				<b><?= number_format($countRecords->countAnimals()) ?></b> enregistrements d'animaux
+			</li>
+			<li>
+				<b><?= number_format($countRecords->countFungi()) ?></b> enregistrements de champignons
+			</li>
+		</div>
+			<?php
+		}
+		else{
+			//Default Language
+			?>
 		<div class="lang en">
 			<h1 class="headline">Natural History</h1>
 			<p>The scientific collections of the New Brunswick Museum are the result of more than a century and a half of collection and study.
-			From our beginning in 1842 as Gesner’s Museum of Natural History the collections have grown from 2,173 items listed in Gesner's catalogue to more than
+			From our beginning in 1842 as Gesner's Museum of Natural History the collections have grown from 2,173 items listed in Gesner's catalogue to more than
 			300,000 natural history specimens.</p>
-		</div>
-		<div class="lang fr">
-			<h1 class="headline">Histoire naturelle</h1>
-			<p>Les collections de recherche scientifiques du Musée du Nouveau-Brunswick sont le résultat d’un travail s’échelonnant sur plus d’un siècle et demi.
-			Depuis ses débuts en 1842, comme Musée Gesner, les collections sont passées de 2 173 objets énumérés dans le catalogue Gesner à plus de 300 000 spécimens aujourd'hui.</p>
 		</div>
 		<div style="width:400px;border: 15px solid #236192;padding:30px;margin:20px auto;">
 			This portal currently includes:
@@ -49,6 +70,9 @@ $countRecords = new CountOccurrenceRecords();
 				<b><?= number_format($countRecords->countFungi()) ?></b> records of fungi
 			</li>
 		</div>
+		<?php
+		}
+		?>
 	</div>
 	<?php
 	include($SERVER_ROOT . '/includes/footer.php');
