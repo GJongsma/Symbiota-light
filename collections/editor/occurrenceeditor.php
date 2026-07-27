f9d07a46d124ceec6b50fadbe01caadb3ebd11b0
Edward Gilbert <egbot@asu.edu>
2026-07-27 10:07:28 -0400

    Update occurrenceeditor.php
    Increase preparation field field within the occurrence editor to 1000 characters

--- collections/editor/occurrenceeditor.php
+++ collections/editor/occurrenceeditor.php
@@ -1406,7 +1406,7 @@ else{
 											<div id="preparationsDiv" class="field-div">
 												<?php echo $LANG['PREPARATIONS']; ?>
 												<a href="#" onclick="return dwcDoc('preparations')" tabindex="-1"><img class="docimg" src="../../images/qmark.png" /></a><br/>
-												<textarea type="text" name="preparations" maxlength="100" onchange="fieldChanged('preparations');" ><?= array_key_exists('preparations',$occArr)?$occArr['preparations']:''; ?></textarea>
+												<textarea type="text" name="preparations" maxlength="1000" onchange="fieldChanged('preparations');" ><?= array_key_exists('preparations',$occArr)?$occArr['preparations']:''; ?></textarea>
 											</div>
 											<div id="reproductiveConditionDiv" class="field-div">
 												<?php echo $LANG['REPRODUCTIVE_CONDITION']; ?>
